f099a5824ab58ffbf7da469d10549c105a2bb702
javar0ne <giuseppe.iavarone19@gmail.com>
2024-10-05 11:00:31 +0200

    feat!: add docker files

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ from config.logging import configure_logging
 app = Flask(__name__)
 configure_logging(app)
 
+@app.route('/')
+def handle_hello_world():
+    return 'Hello World!'
+
 if __name__ == '__main__':
     app.run()
