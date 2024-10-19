4ac5ffa02c39864812e20065cdcade2d1d23ba57
javar0ne <giuseppe.iavarone19@gmail.com>
2024-10-19 15:57:45 +0200

    refactor: replaced marshmallow with pydantic for traveler

--- traveler/model.py
+++ traveler/model.py
@@ -1,67 +1,38 @@
 from datetime import datetime
+from typing import Optional
 
-from marshmallow import Schema, fields
+from pydantic import BaseModel, Field
+
+from common.json_encoders import PyObjectId
 
 COLLECTION_NAME = "travelers"
 
-class Traveler:
-    def __init__(
-        self,
-        phone_number: str = None,
-        currency: str = None,
-        name: str = None,
-        surname: str = None,
-        birth_date: datetime = None,
-        interested_in: list[int] = list,
-        user_id: str = None,
-        _id: str = None
-    ):
-        self.phone_number = phone_number
-        self.currency = currency
-        self.name = name
-        self.surname = surname
-        self.birth_date = birth_date
-        self.interested_in = interested_in
-        self.user_id = user_id
+class TravelerModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    phone_number: str
+    currency: str
+    name: str
+    surname: str
+    birth_date: datetime
+    interested_in: list[int]
+    user_id: Optional[str]
 
-    def to_dict(self):
-        return {
-            'phone_number': self.phone_number,
-            'currency': self.currency,
-            'name': self.name,
-            'surname': self.surname,
-            'birth_date': self.birth_date,
-            'interested_in': self.interested_in,
-            "user_id": self.user_id
-        }
+class TravelerCreateModel(BaseModel):
+    email: str
+    password: str
+    phone_number: str
+    currency: str
+    name: str
+    surname: str
+    birth_date: datetime
+    interested_in: list[int]
+    user_id: Optional[str] = None
 
-class TravelerCreateRequest(Schema):
-    _id = fields.Str(dump_only=True)
-    email = fields.Str(required=True)
-    password = fields.Str(required=True)
-    phone_number = fields.Str(required=True)
-    currency = fields.Str(required=True)
-    name = fields.Str(required=True)
-    surname = fields.Str(required=True)
-    birth_date = fields.DateTime(required=True)
-    interested_in = fields.List(fields.Int(), required=True)
-
-class TravelerUpdateRequest(Schema):
-    _id = fields.Str(dump_only=True)
-    email = fields.Str(required=True)
-    phone_number = fields.Str(required=True)
-    currency = fields.Str(required=True)
-    name = fields.Str(required=True)
-    surname = fields.Str(required=True)
-    birth_date = fields.DateTime(required=True)
-    interested_in = fields.List(fields.Int(), required=True)
-
-class TravelerResponse(Schema):
-    _id = fields.Str(required=True)
-    email = fields.Str(required=True)
-    phone_number = fields.Str(required=True)
-    currency = fields.Str(required=True)
-    name = fields.Str(required=True)
-    surname = fields.Str(required=True)
-    birth_date = fields.DateTime(required=True)
-    interested_in = fields.List(fields.Int(), required=True)
+class TravelerUpdateModel(BaseModel):
+    phone_number: str
+    currency: str
+    name: str
+    surname: str
+    birth_date: datetime
+    interested_in: list[int]
+    user_id: str
