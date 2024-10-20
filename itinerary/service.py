f22b220a0647e7536c20c4604c22c45896232037
javar0ne <giuseppe.iavarone19@gmail.com>
2024-10-20 18:20:27 +0200

    refactor: created service for itinerary, let assistant only interface with chatgpt feat!: added itinerary model and bad gateway response wrapper

---
+++ itinerary/service.py
@@ -0,0 +1,27 @@
+import json
+from typing import Optional
+
+from common.assistant import ask_assistant
+from common.extensions import CITY_KEY_SUFFIX, CITY_DESCRIPTION_SYSTEM_INSTRUCTIONS, DAILY_EXPIRE, \
+    redis_city_description
+from itinerary.model import CityDescription
+
+
+def get_city_description(city: str) -> Optional[CityDescription]:
+    city_key = f"{city.lower().replace(' ', '-')}-{CITY_KEY_SUFFIX}"
+    if redis_city_description.exists(city_key):
+        city_description = json.loads(redis_city_description.get(city_key))
+        return CityDescription(**city_description)
+
+    system_instructions = CITY_DESCRIPTION_SYSTEM_INSTRUCTIONS.copy()
+    system_instructions.append({"role": "user", "content": f"Provide a short description of {city} city." })
+
+    city_description = ask_assistant(system_instructions, CityDescription)
+
+    if city_description is None:
+        return None
+
+    # TODO: create specific redis db for city descriptions
+    redis_city_description.set(city_key, json.dumps(city_description.model_dump()), DAILY_EXPIRE)
+
+    return city_description
