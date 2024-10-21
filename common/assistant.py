d4cacae5b5eff922887ccd6dfcb08cfddfd86208
Giuseppe Iavarone <giuseppe.iavarone@alten.it>
2024-10-21 18:19:23 +0200

    refactor: add Conversation and ConversationRole class to assistant to handle conversations better. converted system instructions from list to dict.

--- common/assistant.py
+++ common/assistant.py
@@ -1,22 +1,50 @@
 import logging
+import re
+from enum import Enum
 
 from common.extensions import assistant, OPENAI_MODEL, MAX_COMPLETION_TOKEN
 
 logger = logging.getLogger(__name__)
 
-def ask_assistant(system_instructions: list[dict], response_format: type):
+class ConversationRole(Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
+    SYSTEM = "system"
+
+class Conversation:
+    def __init__(self, response_format: type):
+        self.response_format = response_format
+        self.messages = []
+
+    def add_message(self, role, content):
+        self.messages.append({
+            'role': role,
+            'content': self.encode(content)
+        })
+
+    def add_message_from(self, element: dict):
+        self.messages.append({
+            'role': element["role"],
+            'content': self.encode(element["content"])
+        })
+
+    @staticmethod
+    def encode(content: str):
+        return re.sub(r'\s+', ' ', content).strip()
+
+def ask_assistant(conversation: Conversation):
     completion = assistant.beta.chat.completions.parse(
         model=OPENAI_MODEL,
-        messages=system_instructions,
-        response_format=response_format,
+        messages=conversation.messages,
+        response_format=conversation.response_format,
     )
 
     return completion.choices[0].message.parsed
 
-def ask_assistant_long_response(messages: list[dict], response_format: type):
+def ask_assistant_long_response(conversation: Conversation, response_format: type):
     completion = assistant.beta.chat.completions.parse(
         model=OPENAI_MODEL,
-        messages=messages,
+        messages=conversation.messages,
         response_format=response_format,
         max_completion_tokens=MAX_COMPLETION_TOKEN
     )
--- itinerary/service.py
+++ itinerary/service.py
@@ -1,14 +1,16 @@
 import json
+import logging
 import threading
 from typing import Optional
 
 from bson import ObjectId
 
-from common.assistant import ask_assistant, logger
+from common.assistant import ask_assistant, Conversation, ConversationRole
 from common.extensions import CITY_KEY_SUFFIX, CITY_DESCRIPTION_SYSTEM_INSTRUCTIONS, DAILY_EXPIRE, \
-    redis_city_description, ITINERARY_SYSTEM_INSTRUCTIONS, db
+    redis_city_description, ITINERARY_SYSTEM_INSTRUCTIONS, db, CITY_DESCRIPTION_USER_PROMPT, ITINERARY_USER_PROMPT
 from itinerary.model import CityDescription, AssistantItineraryResponse, ItineraryRequestStatus
 
+logger = logging.getLogger(__name__)
 
 def get_city_description(city: str) -> Optional[CityDescription]:
     city_key = f"{city.lower().replace(' ', '-')}-{CITY_KEY_SUFFIX}"
@@ -16,10 +18,11 @@ def get_city_description(city: str) -> Optional[CityDescription]:
         city_description = json.loads(redis_city_description.get(city_key))
         return CityDescription(**city_description)
 
-    system_instructions = CITY_DESCRIPTION_SYSTEM_INSTRUCTIONS.copy()
-    system_instructions.append({"role": "user", "content": f"Provide a short description of {city} city." })
+    conversation = Conversation(CityDescription)
+    conversation.add_message_from(CITY_DESCRIPTION_SYSTEM_INSTRUCTIONS)
+    conversation.add_message(ConversationRole.USER.value, CITY_DESCRIPTION_USER_PROMPT.format(city=city))
 
-    city_description = ask_assistant(system_instructions, CityDescription)
+    city_description = ask_assistant(conversation)
 
     if city_description is None:
         return None
@@ -30,38 +33,34 @@ def get_city_description(city: str) -> Optional[CityDescription]:
 
 def generate_itinerary():
     request_id = db["itinerary_requests"].insert_one({"itinerary": [], "status": ItineraryRequestStatus.PENDING.name}).inserted_id
-    messages = ITINERARY_SYSTEM_INSTRUCTIONS.copy()
+    conversation = Conversation(AssistantItineraryResponse)
+    conversation.add_message_from(ITINERARY_SYSTEM_INSTRUCTIONS)
 
-    threading.Thread(target=generate_day_by_day, args=(messages, request_id)).start()
+    threading.Thread(target=generate_day_by_day, args=(conversation, request_id)).start()
 
     return request_id
 
-def generate_day_by_day(messages: list[dict], request_id: ObjectId):
-    # TODO: refactor
+def generate_day_by_day(conversation: Conversation, request_id: ObjectId):
+    logger.info("starting itinerary generation..")
     for day in range(1,5):
         try:
             logger.info("starting day %d", day)
-            messages.append(
-                {
-                    "role": "user",
-                    "content": f"i'm going to visit London for 4 days in December, with a range budget per person of 0 to max 500 EUR with my family. i'm interested into food exploration and nightlife. generate the itinerary for day {day}."
-                }
+            conversation.add_message(ConversationRole.USER.value, ITINERARY_USER_PROMPT.format(day=day))
+
+            itinerary_response = ask_assistant(conversation)
+
+            db["itinerary_requests"].update_one(
+                {"_id": request_id},
+                {"$push": {"itinerary": itinerary_response.itinerary[0].model_dump()}}
             )
 
-            itinerary_response = ask_assistant(messages, AssistantItineraryResponse)
-
-            db["itinerary_requests"].update_one({"_id": request_id}, {"$push": {"itinerary": itinerary_response.itinerary[0].model_dump()}})
-
-            messages.append(
-                {
-                    "role": "assistant",
-                    "content": f"{itinerary_response.model_dump()}"
-                }
-            )
-
+            conversation.add_message(ConversationRole.ASSISTANT.value, f"{itinerary_response.model_dump()}")
             logger.info("completed day %d", day)
         except Exception as err:
             logger.error(err)
             db["itinerary_requests"].update_one({"_id": request_id},{"$set": {"status": ItineraryRequestStatus.ERROR.name}})
+            logger.info("error on day %d", day)
+            logger.info("stopped itinerary generation!")
 
     db["itinerary_requests"].update_one({"_id": request_id}, {"$set": {"status": ItineraryRequestStatus.COMPLETED.name}})
+    logger.info("completed itinerary generation!")
