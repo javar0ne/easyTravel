d9e52bc1ace4cbf58c75af91ad7c75161637fa41
Giuseppe Iavarone <54623419+javar0ne@users.noreply.github.com>
2024-12-02 00:59:02 +0100

    Llink traveler signup page (#26)
    * feat!: linked traveler signup page to BE
    
    * fix: login page style

--- app/blueprints/traveler/model.py
+++ app/blueprints/traveler/model.py
@@ -27,11 +27,11 @@ class TravelerBaseModel(BaseModel):
 class CreateTravelerRequest(BaseModel):
     email: str
     password: str
-    phone_number: str
     currency: str
-    name: str
-    surname: str
+    first_name: str
+    last_name: str
     birth_date: datetime
+    phone_number: Optional[str] = None
     user_id: Optional[str] = None
 
 class ConfirmTravelerSignupRequest(TravelerBaseModel):
@@ -39,22 +39,22 @@ class ConfirmTravelerSignupRequest(TravelerBaseModel):
     token: str
 
 class UpdateTravelerRequest(TravelerBaseModel):
-    phone_number: str
     currency: str
-    name: str
-    surname: str
+    first_name: str
+    last_name: str
     birth_date: datetime
     interested_in: list[str]
+    phone_number: Optional[str] = None
 
 class Traveler(TravelerBaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
-    phone_number: str
     currency: str
-    name: str
-    surname: str
+    first_name: str
+    last_name: str
     birth_date: datetime
     user_id: str
     interested_in: Optional[list[str]] = []
+    phone_number: Optional[str] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
     deleted_at: Optional[datetime] = None
@@ -64,8 +64,8 @@ class Traveler(TravelerBaseModel):
         return Traveler(
             phone_number=traveler.phone_number,
             currency=traveler.currency,
-            name=traveler.name,
-            surname=traveler.surname,
+            first_name=traveler.first_name,
+            last_name=traveler.last_name,
             birth_date=traveler.birth_date,
             user_id=traveler.user_id,
         )
@@ -73,7 +73,7 @@ class Traveler(TravelerBaseModel):
     def update_by(self, update_traveler_req: UpdateTravelerRequest):
         self.phone_number = update_traveler_req.phone_number
         self.currency = update_traveler_req.currency
-        self.name = update_traveler_req.name
-        self.surname = update_traveler_req.surname
+        self.first_name = update_traveler_req.first_name
+        self.last_name = update_traveler_req.last_name
         self.birth_date = update_traveler_req.birth_date
         self.interested_in = update_traveler_req.interested_in
